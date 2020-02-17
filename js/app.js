75b26a52c5a18965a83be8b786d8d047b9b0d71c
Ammarhr <hariry49@gmail.com>
2020-02-17 17:12:22 +0200

    the half of lab 2

---
+++ js/app.js
@@ -0,0 +1,70 @@
+`use instrict`;
+alert("Let's get to know each other.");
+var myNationality = prompt(" my nationality is syrian?", "yes, no , y, n");
+switch (myNationality.toLowerCase()) {
+    case myNationality == 'yes':
+    case myNationality == 'y':
+        alert("you are write!");
+        break;
+    case myNationality == 'n':
+    case myNationality == 'no':
+        alert("you are write!");
+        break
+    default:
+        alert("please type the answer!");
+}
+var myAge = prompt("Am I 35 years old?", "yes, no , y, n");
+switch (myAge.toLowerCase()) {
+    case myAge == 'no':
+    case myAge == 'n':
+        alert("you are write!");
+        break;
+    case myAge = 'yes':
+    case myAge = 'y':
+        alert("you are write!");
+        break;
+    default:
+        alert("please type the answer!");
+}
+var myGrads = prompt("my job is football player?", "yes, no , y, n");
+switch (myGrads.toLowerCase()) {
+    case myGrads === "no":
+    case myGrads === "n":
+        alert("you are write!");
+        break;
+    case myGrads === "y":
+    case myGrads === "yes":
+        alert("you are write!");
+        break;
+    default:
+        alert("please type the answer!");
+}
+var myClub = prompt("my fav club is barcelona?", "yes, no , y, n");
+switch (myClub.toLowerCase()) {
+    case myClub = 'yes':
+    case myClub = 'y':
+        alert("you are write!");
+        break;
+    case myClub = 'n':
+    case myClub = 'no':
+        alert("you are write!");
+        break;
+    default:
+        alert("please type the answer!");
+}
+var favFilm = prompt("my fav film is Batman?", "yes, no , y, n");
+switch (favFilm.toLowerCase()) {
+    case favFilm = 'no':
+    case favFilm = 'n':
+        alert("you are write!");
+        break;
+    case favFilm = 'y':
+    case favFilm = 'yes':
+        alert("you are write!");
+        break;
+
+    default:
+        alert("please type the answer!");
+}
+var yourName = prompt("what's your name?");
+alert("good job and welcome" + yourName);
