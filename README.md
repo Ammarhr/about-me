be9296877bb9961ec9e9f9f871151d81e0d7f80e
Ammarhr <hariry49@gmail.com>
2020-02-17 23:01:25 +0200

    add html, css and java script stuff for lab 02

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # about-me
-this is a repo is about me, it will contain an info-graphic of me and a little fun with a guessing game  , it is for the lab 02
+this is a repo is about me, it will contain an info-graphic of me and a little fun with a guessing game  , it is for the lab 02.
+
+here I design a web page contains a pref informations about me using html, css and javascript.
--- js/app.js
+++ js/app.js
@@ -1,70 +1,71 @@
-`use instrict`;
+`use strict`
 alert("Let's get to know each other.");
+var yourName = prompt("what's your name?");
 var myNationality = prompt(" my nationality is syrian?", "yes, no , y, n");
 switch (myNationality.toLowerCase()) {
-    case myNationality == 'yes':
-    case myNationality == 'y':
+    case myNationality = "yes":
+    case myNationality = "y":
         alert("you are write!");
         break;
-    case myNationality == 'n':
-    case myNationality == 'no':
-        alert("you are write!");
+    case myNationality = "n":
+    case myNationality = "no":
+        alert("wrong answer!");
         break
     default:
         alert("please type the answer!");
 }
 var myAge = prompt("Am I 35 years old?", "yes, no , y, n");
 switch (myAge.toLowerCase()) {
-    case myAge == 'no':
-    case myAge == 'n':
+    case myAge = "no":
+    case myAge = "n":
         alert("you are write!");
         break;
-    case myAge = 'yes':
-    case myAge = 'y':
-        alert("you are write!");
+    case myAge = "yes":
+    case myAge = "y":
+        alert("wrong answer!");
         break;
     default:
         alert("please type the answer!");
 }
 var myGrads = prompt("my job is football player?", "yes, no , y, n");
 switch (myGrads.toLowerCase()) {
-    case myGrads === "no":
-    case myGrads === "n":
+    case myGrads = "no":
+    case myGrads = "n":
         alert("you are write!");
         break;
-    case myGrads === "y":
-    case myGrads === "yes":
-        alert("you are write!");
+    case myGrads = "y":
+    case myGrads = "yes":
+        alert("wrong answer!");
         break;
     default:
         alert("please type the answer!");
 }
 var myClub = prompt("my fav club is barcelona?", "yes, no , y, n");
 switch (myClub.toLowerCase()) {
-    case myClub = 'yes':
-    case myClub = 'y':
+    case myClub = "yes":
+    case myClub = "y":
         alert("you are write!");
         break;
-    case myClub = 'n':
-    case myClub = 'no':
-        alert("you are write!");
+    case myClub = "n":
+    case myClub = "no":
+        alert("wrong answer");
         break;
     default:
         alert("please type the answer!");
 }
 var favFilm = prompt("my fav film is Batman?", "yes, no , y, n");
 switch (favFilm.toLowerCase()) {
-    case favFilm = 'no':
-    case favFilm = 'n':
+    case favFilm = "no":
+    case favFilm = "n":
         alert("you are write!");
         break;
-    case favFilm = 'y':
-    case favFilm = 'yes':
-        alert("you are write!");
+    case favFilm = "y":
+    case favFilm = "yes":
+        alert("wrong answer!");
         break;
 
     default:
         alert("please type the answer!");
 }
-var yourName = prompt("what's your name?");
-alert("good job and welcome" + yourName);
+var yourName ;
+alert("good job and welcome ^-^ " + yourName);
