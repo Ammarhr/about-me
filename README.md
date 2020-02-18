fe2eaee88cac722f07dd2d62dd62f4feeeecacff
Ammarhr <hariry49@gmail.com>
2020-02-19 00:17:47 +0200

    add new quatsions with js and lists in html

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # about-me
-this is a repo is about me, it will contain an info-graphic of me and a little fun with a guessing game  , it is for the lab 02.
+this is a repo is about me, it will contain an info-graphic of me and a little fun with a guessing game  , it is for the lab 02 and lab 03 using loops.
 
 here I design a web page contains a pref informations about me using html, css and javascript.
+................
+for lab 03 adding a new tow quatsions with many attampts and give a score for the correct answers.
+adding orderd and unorderd lists containe a new informations of myself. give tis new elements a style withe css.
--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 `use strict`
 alert("Let's get to know each other.");
 var yourName = prompt("what's your name?");
+alert('welcome ' + yourName);
+var score = 0;
 var myNationality = prompt(" my nationality is syrian?", "yes, no , y, n");
 switch (myNationality.toLowerCase()) {
     case myNationality = "yes":
     case myNationality = "y":
         alert("you are write!");
+        score = score + 1;
         break;
     case myNationality = "n":
     case myNationality = "no":
         alert("wrong answer!");
+        score = score + 1;
         break
     default:
         alert("please type the answer!");
@@ -19,6 +23,7 @@ switch (myAge.toLowerCase()) {
     case myAge = "no":
     case myAge = "n":
         alert("you are write!");
+        score = score + 1;
         break;
     case myAge = "yes":
     case myAge = "y":
@@ -32,6 +37,7 @@ switch (myGrads.toLowerCase()) {
     case myGrads = "no":
     case myGrads = "n":
         alert("you are write!");
+        score = score + 1;
         break;
     case myGrads = "y":
     case myGrads = "yes":
@@ -45,6 +51,7 @@ switch (myClub.toLowerCase()) {
     case myClub = "yes":
     case myClub = "y":
         alert("you are write!");
+        score = score + 1;
         break;
     case myClub = "n":
     case myClub = "no":
@@ -58,6 +65,7 @@ switch (favFilm.toLowerCase()) {
     case favFilm = "no":
     case favFilm = "n":
         alert("you are write!");
+        score = score + 1;
         break;
     case favFilm = "y":
     case favFilm = "yes":
@@ -67,5 +75,50 @@ switch (favFilm.toLowerCase()) {
     default:
         alert("please type the answer!");
 }
-var yourName ;
-alert("good job and welcome ^-^ " + yourName);
+var numLang = Number(prompt('who many language I talk? 1, 2, 3, or 4'))
+var guessNum = 0;
+
+if (numLang == 3) {
+    alert("you are write! good jooob dude. we have one more qatstion left!");
+    score = score + 1;
+} else {
+    alert("wrong! try again");
+    for (guessNum; guessNum < 3; guessNum++) {
+        numLang = prompt('who many language I talk? 1, 2, 3, or 4');
+        if (numLang > 3) {
+            alert("too high");
+        } else if (numLang < 3) {
+            alert("too low");
+        } else {
+            alert("you are write! good jooob dude. we have one more qatstion left!");
+            score = score + 1;
+            break;
+        }
+
+    }
+}
+
+
+var newGus = 0;
+var myChar = ['batman', 'superman', 'joker', 'harley', 'darkside', 'flash'];
+loop: {
+    for (newGus; newGus < myChar.length; newGus++) {
+        var plyerGus = prompt('What\'s my fav DC characters?');
+        switch (plyerGus.toLowerCase()) {
+            case myChar[0]:
+            case myChar[1]:
+            case myChar[2]:
+            case myChar[3]:
+            case myChar[4]:
+            case myChar[5]:
+                alert("you are amazing! really " + plyerGus + " is on of my fav. goooood job");
+                score = score + 1;
+                break loop;
+            default:
+                alert("wrong!! try again!");
+                break;
+        }
+    }
+}
+alert("the correct answers 6th and 7th quastions is " + '3 for the 6th and ' + myChar + " for the 7th");
+alert('your score is ' + score + 'of 7' + " thank you for your time");
